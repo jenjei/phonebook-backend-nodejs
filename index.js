c828d69293690746553a50aa0e21700ec021ef61
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-11-22 14:46:20 +0200

    Exercise 3.13

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-console.log('hello backend')
-
+const Person = require('./models/person')
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const morgan = require('morgan') // https://github.com/expressjs/morgan documentation here
 const cors = require('cors')
 const app = express()
@@ -10,58 +11,32 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan('tiny')) // tiny format with custom token :body
 
-let persons = [
-  { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
-  },
-  { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-  },
-  { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<p>helllooooo</p>')
 })
   
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 app.get('/info', (request, response) => {
-  response.send('<p>Phonebook has info for ' + persons.length + ' people.</p>'
+  response.send('<p>Phonebook has info for ' + Person.length + ' people.</p>'
   + '<p></p>' + Date())
 })
 
-// creating api endpoint for one contact
+// creating endpoint for one contact
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id) // Number for turning string to int
-  const person = persons.find(person => person.id === id)
-    
-  if (person) {
+  Person.findById(request.params.id).then(person => {
       response.json(person)
-    } else {
-      response.status(404).end() // id not found :(
-    }
+    })
 })
 
 // deleting one contact
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  Person = Person.filter(person => person.id !== id)
 
   response.status(204).end()
 })
@@ -77,31 +52,31 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name) { // jos nimi puuttuu, niin anna error viesti
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
+    return response.status(400).json({ error: 'name missing' })
   }
 
   if (!body.number) { // jos numero puuttuu, niin anna error viesti
-    return response.status(400).json({
-      error: 'number missing'
-    })
+    return response.status(400).json({ error: 'number missing' })
   }
 
-  const found = persons.find(person => person.name===body.name)
+  /*const found = Person.find(person => person.name===body.name)
   if(found) {
     return response.status(400).json({
       error: 'name must be unique'
     })
-  }
+  }*/
   
-  const person = {
+  const person = new Person ({
     name: body.name,
     number: body.number,
     id: generateId(5, 100000000)
-  }
+  })
 
-  persons = persons.concat(person)
+  person.save().then(result => {
+    console.log('added ', result.name, result.number, ' to the phonebook')
+    response.json(result)
+    mongoose.connection.close()
+  })
 
   // console.log(person) -> unnecessary when using morgan
   response.json(person)
