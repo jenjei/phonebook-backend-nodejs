5f2e88c70e2f571360980841f21fe8e46bea4922
jenniaylis <62026534+jenniaylis@users.noreply.github.com>
2022-11-22 15:59:34 +0200

    Exercise 3.15

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(morgan('tiny')) // tiny format with custom token :body
 
 app.get('/', (request, response) => {
@@ -28,17 +28,27 @@ app.get('/info', (request, response) => {
 
 // creating endpoint for one contact
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-      response.json(person)
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({error: 'malformatted id'})
     })
 })
 
 // deleting one contact
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  Person = Person.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = (min, max) => {
@@ -81,6 +91,17 @@ app.post('/api/persons', (request, response) => {
     response.json(result)
   })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
